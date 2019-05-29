3e2933663640f567ca9d6cd07769ad7ef240b266
Alexander Bakhtin <grandiozius@gmail.com>
2019-05-29 18:42:43 +0600

    Updates

---
+++ precache-manifest.8474f78a68212cd6aa195c82cae9854e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aec4bbf1ae0dc66ddb93fa8fedd0e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f7780cf5b061c8f2a84",
+    "url": "/static/css/main.fcb48c17.chunk.css"
+  },
+  {
+    "revision": "cc30ee79e107ebf33c91",
+    "url": "/static/js/2.ab2d1335.chunk.js"
+  },
+  {
+    "revision": "3f7780cf5b061c8f2a84",
+    "url": "/static/js/main.4eafde77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
